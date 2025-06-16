42d1f16316f67e21ee3231f6cb4ea87ef3789039
pardan <pardanjamaludin31@gmail.com>
2025-06-16 17:44:45 +0700

    initiate database

--- backend/sql/01_init.sql
+++ backend/sql/01_init.sql
@@ -1,8 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
 CREATE TABLE sensor_data (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    device_id VARCHAR(24) NOT NULL,
-    metric_type VARCHAR(12) NOT NULL,
-    value DOUBLE PRECISION NOT NULL
+    device_id TEXT NOT NULL,
+    metric_type TEXT NOT NULL,
+    value DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (time, id)
 );
+
 SELECT create_hypertable('sensor_data', 'time');
